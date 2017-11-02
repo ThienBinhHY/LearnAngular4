1e15215c7404397c8aea32e1bf23f54050b330d6
quydt <daoquy.uneti@gmail.com>
2017-11-02 14:46:25 +0700

    viết riêng ContactsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';// để sd ngFor ngIf...
 import { Routes, RouterModule } from '@angular/router';
 
-import { ContactsComponent } from './contacts/contacts.component';
+// import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsModule } from './contacts/contacts.module';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routesConfig: Routes = [
-    { path: 'contacts', component: ContactsComponent},
+    // { path: 'contacts', component: ContactsComponent},// se viet rieng ContactModule nên se k dung ở day nữa
     // { path: 'detail', component: ContactDetailComponent},
     { path: 'detail/:id/:name/:phoneNumber', component: ContactDetailComponent },
     { path: '', redirectTo: '/contacts',pathMatch: 'full'},
@@ -18,9 +19,10 @@ const routesConfig: Routes = [
     imports: [
         RouterModule.forRoot(routesConfig),
         CommonModule,
+        ContactsModule
     ],
     declarations: [
-        ContactsComponent,
+        // ContactsComponent,
         ContactDetailComponent,
         PageNotFoundComponent,
     ],
